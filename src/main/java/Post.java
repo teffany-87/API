a6538a01868b025d08b25b91600b523eaad948d0
Tatiana Korysheva <korosteleva87@gmail.com>
2023-06-06 12:15:36 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+    public FormDate birthday;
+}
